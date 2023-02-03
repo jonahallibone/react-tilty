2b02fc3dc197d78f6ab98d61408c70c1a378728e
Austin Jackson <themacphage@gmail.com>
2023-02-02 18:17:45 -0600

    made props optional

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,28 +2,28 @@ import type { ReactNode, CSSProperties } from 'react'
 import type { Property } from 'csstype'
 
 export interface TiltyProps {
-  style: CSSProperties;
-  className: string | Property.AnimationTimingFunction
-  reverse: boolean;
-  max: number;
-  perspective: number;
-  easing: Property.AnimationTimingFunction;
-  scale: number;
-  speed: number;
-  axis: 'X' | 'Y' | null;
-  reset: boolean;
-  glare: boolean;
-  maxGlare: number;
-  glareStyle: CSSProperties;
-  gyroscope: boolean;
-  gyroscopeMinAngleX: number;
-  gyroscopeMaxAngleX: number;
-  gyroscopeMinAngleY: number;
-  gyroscopeMaxAngleY: number;
-  onMouseEnter: () => {};
-  onMouseMove: () => {};
-  onMouseLeave: () => {};
-  onTiltChange: () => {};
+  style?: CSSProperties;
+  className?: string | Property.AnimationTimingFunction
+  reverse?: boolean;
+  max?: number;
+  perspective?: number;
+  easing?: Property.AnimationTimingFunction;
+  scale?: number;
+  speed?: number;
+  axis?: 'X' | 'Y' | null;
+  reset?: boolean;
+  glare?: boolean;
+  maxGlare?: number;
+  glareStyle?: CSSProperties;
+  gyroscope?: boolean;
+  gyroscopeMinAngleX?: number;
+  gyroscopeMaxAngleX?: number;
+  gyroscopeMinAngleY?: number;
+  gyroscopeMaxAngleY?: number;
+  onMouseEnter?: () => {};
+  onMouseMove?: () => {};
+  onMouseLeave?: () => {};
+  onTiltChange?: () => {};
   children: ReactNode;
 }
 
