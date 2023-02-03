6944c421895a02bf47a9c1fd4175c2ab8d13024a
Austin Jackson <themacphage@gmail.com>
2023-02-02 18:00:11 -0600

    added typescript typing

---
+++ src/index.d.ts
@@ -0,0 +1,30 @@
+import type { ReactNode, CSSProperties } from 'react'
+import type { Property } from 'csstype'
+
+export interface TiltyProps {
+  style: CSSProperties;
+  className: string | Property.AnimationTimingFunction
+  reverse: boolean;
+  max: number;
+  perspective: number;
+  easing: Property.AnimationTimingFunction;
+  scale: number;
+  speed: number;
+  axis: 'X' | 'Y' | null;
+  reset: boolean;
+  glare: boolean;
+  maxGlare: number;
+  glareStyle: CSSProperties;
+  gyroscope: boolean;
+  gyroscopeMinAngleX: number;
+  gyroscopeMaxAngleX: number;
+  gyroscopeMinAngleY: number;
+  gyroscopeMaxAngleY: number;
+  onMouseEnter: () => {};
+  onMouseMove: () => {};
+  onMouseLeave: () => {};
+  onTiltChange: () => {};
+  children: ReactNode;
+}
+
+export default function Tilty(props: TiltyProps): JSX.Element;
